6f6dfaebc5cc05e217bd9b701266a9e1a8067e1f
Ravindra Dhore <ravindrad@ansh.com>
2019-11-26 16:22:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularDraggableModule } from 'angular2-draggable';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularDraggableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
